7ee786fba12737e86b9bb3bba26f822b6cc9364b
Joe Hughes <jjhughes57@gmail.com>
2021-12-07 16:00:33 -0500

    formatting

--- main.ts
+++ main.ts
@@ -1,12 +1,11 @@
-import {V1Deployment, useGetDeploymentQuery} from './types';
+import { V1Deployment, useGetDeploymentQuery } from "./types";
 async function main() {
-    useGetDeploymentQuery({onCompleted: (data) => {
-let foo: V1Deployment;
-foo = data.getDeployment
-
-
-    }});
-
-    }
+    useGetDeploymentQuery({
+        onCompleted: (data) => {
+            let foo: V1Deployment;
+            foo = data.getDeployment;
+        },
+    });
+}
 
 main();
